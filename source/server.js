26beb1bece4e52a9657bdc68e5db521343c29cb0
SishwaThoka <thoka.s@northeastern.edu>
2023-04-06 12:34:19 -0400

    Assignment8

--- source/server.js
+++ source/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const client = require('./logger/statsd');
 const router = express.Router();
 const logger = require('./logger/logger');
 console.log("Please here me - server.js");
 logger.info("Please here me - server.js");
 router.get('/healthz', (req, res) => {
+  client.increment('Gethealthz', 1)
     console.log('inside get request');
     logger.info('inside get request');
     res.send({"status":'ok'});
